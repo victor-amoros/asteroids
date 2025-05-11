9da638646c1c5621de1de66628a122ac0f8ab0c3
victor-amoros <victor.amoros96@gmail.com>
2025-05-11 19:28:45 +0200

    created player sprite and added rotation

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 import pygame
 from constants import *
+from circleshape import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        player.update(dt)
+        
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
+        
+        #limit to 60 FPS
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
